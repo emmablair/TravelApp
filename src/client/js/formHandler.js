eb9800a48ee690cdd060631bf5cfd96170889395
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-13 18:51:05 -0400

    formhandler ( fetch user input)

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,24 @@
+const formHandler = async(e) => {
+    e.preventDefault();
+    const userInput = document.querySelector('#userInput').value;
+    console.log('::: FORM SUBMITTED | GEONAMES :::')
+    // API call
+    const response = await fetch('/geoname',  {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input: userInput })
+    })
+    .then(res => {
+        const postData = res.json();
+        return postData;
+    })
+    .catch((error) => {
+        console.log('HANDLE SUBMIT promise error', error);
+    });
+    console.log(response.lat);
+    console.log('done')
+}
