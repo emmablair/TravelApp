997c7800cd3bd2dd3c32104f784747bd3bcdbeaf
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-14 17:52:41 -0400

    exact day future forcast

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,23 @@
+// ::: Current date in date input field :::
+Date.prototype.toDateInputValue = (function() {
+    // allow correct timezone
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+// apply current date
+const currentDate = new Date().toDateInputValue();
+document.querySelector('#dateInput').value = currentDate; //.valueAsDate without timezone also works
+
+//::: date diffence for weather Forcast :::
+const dateDifference = (date1, date2) => {
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+}
+
+
+// ::: form Handler Func :::
 const formHandler = async(e) => {
     e.preventDefault();
     const userInput = document.querySelector('#userInput').value;
@@ -21,10 +41,17 @@ const formHandler = async(e) => {
     });
     console.log(geoname);
     console.log('done')
+    getWeather(geoname)
+}
 
-    const dateInput = document.querySelector('#dateInput').value;
-    console.log(dateInput);
+const getWeather = async(geoname) => {
     // ::: WEATHERBIT :::
+    const dateInput = document.querySelector('#dateInput').value;
+    console.log(`current date:${currentDate}`);
+    console.log(`destination date:${dateInput}`);
+    const forcastDay = dateDifference(currentDate, dateInput);
+    console.log(`destination day difference:${forcastDay}`);
+    
     const weatherbit = await fetch('/weatherbit',  {
         method: 'POST',
         credentials: 'same-origin',
@@ -32,7 +59,8 @@ const formHandler = async(e) => {
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ 
+        body: JSON.stringify({
+            day: forcastDay, 
             lat: geoname.lat,
             lng: geoname.lng 
         })
@@ -44,15 +72,8 @@ const formHandler = async(e) => {
     .catch((error) => {
         console.log('WEATHERBIT promise error', error);
     });
-    console.log('::: FORM SUBMITTED | WEATHERBIT :::')
+    console.log('::: FORM SUBMITTED | WEATHERBIT :::');
+    console.log(weatherbit.data);
+    console.log(weatherbit.data[forcastDay]);
+    document.querySelector('#weatherIcon').src = `https://www.weatherbit.io/static/img/icons/${weatherbit.data[forcastDay].weather.icon}.png`;
 }
-
-// Current date in date input field
-Date.prototype.toDateInputValue = (function() {
-    // allow correct timezone
-    var local = new Date(this);
-    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-    return local.toJSON().slice(0,10);
-});
-// apply current date
-document.querySelector('#dateInput').value = new Date().toDateInputValue(); //.valueAsDate without timezone also works
