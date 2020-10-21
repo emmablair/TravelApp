6cc8ec2a90e0cf547472f1f157269a2201541103
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-20 21:56:50 -0400

    save too localStorage only at click of SAVE button

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -48,30 +48,34 @@ const formHandler = async(e) => {
     console.log(tripInfo);
     console.log('::: SUCCESSFUL POST | Completed tripInfo :::')
     // save(e, tripInfo);
-    storage(tripInfo);
-    updateUI(tripInfo);
+    // storage(tripInfo);
+    // updateUI(tripInfo);
 }
 
-const storage = (tripInfo) => {
-    let tripInfos;
-    if(localStorage.getItem('tripInfos') === null) {
-        tripInfos = [];
+const storage = (trip) => {
+    let trips;
+    if(localStorage.getItem('trips') === null) {
+        trips = [];
     } else {
-        tripInfos = JSON.parse(localStorage.getItem('tripInfos'));
+        trips = JSON.parse(localStorage.getItem('trips'));
+        // addSave(trips);
     }
-    tripInfos.push(tripInfo);
-    localStorage.setItem('tripInfos', JSON.stringify(tripInfos));
-    console.log(tripInfo.arrival)
+    trips.push(trip);
+    localStorage.setItem('trips', JSON.stringify(trips));
+    // addSave(trips);
+    addSave(trip);
+    // console.log(trip.arrival)
 }
+// GO BACK AND GET FOREACH LOCAL STORAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// TO MAKE SURE ALL OLD INFO IS SHOWN AFTER LOAD AND REFRESH @ WINDOW OPENING
+
+// const tripInfos = JSON.parse(localStorage.getItem('tripInfos'));
+// console.log(tripInfos[0])
 
 
-const tripInfos = JSON.parse(localStorage.getItem('tripInfos'));
-console.log(tripInfos[0])
-
-
-const updateUI = (tripInfo) => {
-    console.log(tripInfo.departure.day)
-}
+// const updateUI = (tripInfo) => {
+//     console.log(tripInfo.departure.day)
+// }
 
 const save = async() => {
     document.querySelector('#save').addEventListener('click', async() => {
@@ -87,9 +91,20 @@ const save = async() => {
         try{
             const allData = await trip.json();
             console.log(allData)
+            storage(allData);
+            // addSave(allData);
         } catch (error) {
             console.log('error', error);
         }
+        // storage(trip);
     })
+    
 }
 save()
+
+const addSave = (allData) => {
+    let savedTrips = document.querySelector('#savedTrips');
+    let div = document.createElement('div');
+    savedTrips.appendChild(div)
+    div.innerHTML = `${allData.arrival.at}`  
+}
