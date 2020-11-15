036ab7a00619cd8049a8d3f3a5f53e1538fdba2d
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-11-14 23:27:25 -0500

    Create toTop.js

---
+++ src/client/js/toTop.js
@@ -0,0 +1,18 @@
+/* ::: Scroll to Top ::: */
+
+function backToTop() {
+    const toTopButton = document.querySelector('#scrollToTop');
+    document.addEventListener('scroll', (event) => {  
+        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+            toTopButton.style.display = 'block';
+        } else {
+            toTopButton.style.display = 'none';
+        }
+    })
+    toTopButton.addEventListener('click', (event) => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+}
+
+backToTop();
