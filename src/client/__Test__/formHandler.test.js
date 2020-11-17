2f016f2691643f7c68d1639c8b49ae6020d79b6d
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-11-17 17:04:21 -0500

    jest test

---
+++ src/client/__Test__/formHandler.test.js
@@ -0,0 +1,7 @@
+import { formHandler } from '../js/formHandler';
+
+describe('Handle submission for geoNames Weatherbit and Pixabay API', () => {
+    test('grab all API info & handle submission functions', () => {
+        expect(formHandler).toBeDefined();
+    });
+});
---
+++ src/client/__Test__/toTop.test.js
@@ -0,0 +1,7 @@
+import { backToTop } from '../js/toTop';
+
+describe('on click scroll back to top', () => {
+    test('scroll to top', () => {
+        expect(backToTop).toBeDefined();
+    });
+});
