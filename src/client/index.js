725817402ee2fd2d0fd94cb4a829a13b6a9dcdf1
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-11-17 17:04:30 -0500

    Create index.js

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+import { formHandler } from './js/formHandler.js'
+import { backToTop } from './js/toTop.js'
+
+import './styles/about.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/icons.scss'
+import './styles/index.scss'
+import './styles/modal.scss'
+import './styles/nav.scss'
+
+import './images/travel_road.jpg';
+
+export {
+    formHandler,
+    backToTop
+}
